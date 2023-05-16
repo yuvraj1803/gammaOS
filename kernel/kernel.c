accab4bda7714b151e9accdd3a584cffe1408409
Yuvraj Sakshith <ysakshith@gmail.com>
2023-05-16 20:27:43 +0530

    kernel file cleanup

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -22,6 +22,7 @@
 #include "kstatus.h"
 #include "task/process/process.h"
 #include "task/task.h"
+#include "task/process/process.h"
 #include "idt/__0x80/__0x80.h"
 #include "../drivers/keyboard/keyboard.h"
 
@@ -56,84 +57,72 @@ void kernel_start(){
     // form gdt
     memset(_gdt, 0, sizeof(struct  gdt));
     get_gdt_from_structured(_gdt, _gdt_s, GDT_ENTRIES);
-
-    kputs("[KERNEL] setting up gdt.\n");
+    kputs("[KERNEL]: setting up gdt.\n");
 
     // load gdt
     load_gdt(_gdt, sizeof(_gdt));
-
-    kputs("[KERNEL] loaded up gdt.\n");
+    kputs("[KERNEL]: loaded up gdt.\n");
 
     // form tss
     memset(&_tss, 0, sizeof(_tss));
     _tss.esp0 = 0x600000;  
     _tss.ss0  = KERNEL_DATA_SELECTOR;
-
-    kputs("[KERNEL] setting up tss.\n");
+    kputs("[KERNEL]: setting up tss.\n");
 
 
     // load tss
     tss_load(0x28); // 0x28 is the offset of tss in gdt
-
-    kputs("[KERNEL] loaded up tss.\n");
+    kputs("[KERNEL]: loaded up tss.\n");
 
 
     idt_init(); // initialise interrupt descriptor table
-
-    kputs("[KERNEL] initialised idt. \n");
+    kputs("[KERNEL]: initialised idt. \n");
 
     __0x80_init(); // initialise 0x80 int
-
-    kputs("[KERNEL] initialised gammaOS system call handlers. \n");
+    kputs("[KERNEL]: initialised gammaOS system call handlers. \n");
 
     kheap_init(); // initialise kernel heap section
+    kputs("[KERNEL]: initialised kernel heap. \n");
 
-    kputs("[KERNEL] initialised kernel heap. \n");
+    process_init();
+    kputs("[KERNEL]: initialised process structures\n");
 
     keyboard_init();// intialise the abstract keyboard driver
-
-    kputs("[KERNEL] initialised keyboard drivers. \n");
+    kputs("[KERNEL]: initialised keyboard drivers. \n");
 
     /* creating kernel address space and loading its page directory into cr3*/
     kernel_space = create_virtual_address_space(PAGE_PRESENT | PAGE_USER_ACCESS | PAGE_WRITE_ACCESS);
     if(kernel_space == 0x0){
         kpanic("[KERNEL ERROR]: Couldn't create kernel address space...\n");
     }
-
-    kputs("[KERNEL] initialised kernel address space. \n");
+    kputs("[KERNEL]: initialised kernel address space. \n");
 
     load_page_directory(kernel_space->pd);
-
-    kputs("[KERNEL] loaded kernel page directory. \n");
+    kputs("[KERNEL]: loaded kernel page directory. \n");
 
     file_init(); // initialise the virtual file system layer
-
-    kputs("[KERNEL] initialised virtual filesystem. \n");
+    kputs("[KERNEL]: initialised virtual filesystem. \n");
 
     if(vfs_attach(fat16_init()) < 0){// attach fat16 fs into the primary disk
         kpanic("[KERNEL ERROR]: Couldn't attach filesystem to primary disk");
     } 
-
-    kputs("[KERNEL] attached fat16 driver. \n");
+    kputs("[KERNEL]: attached fat16 driver. \n");
 
     if(!disk_init('A')){
         kpanic("[KERNEL ERROR]: Couldn't initialise primary disk...\n");
     }
-
-    kputs("[KERNEL] initialised primary disk. \n");
+    kputs("[KERNEL]: initialised primary disk. \n");
 
     enable_paging(); // enable paging
-
-    kputs("[KERNEL] initialised paging. \n");
+    kputs("[KERNEL]: initialised paging. \n");
 
 
     // set-up gshell
     struct process* proc_gshell = process_new("A:/usrspc/gshell/main.o");
-
-    kputs("[KERNEL] loading gshell...\n");
+    kputs("[KERNEL]: loading gshell...\n");
 
     if(!proc_gshell){
-        kpanic("process not created!!:(\n");
+        kpanic("[[KERNEL ERROR]: process not created!!:(\n");
     }   
 
     __enable_irq(); // enable interrupts
