29772a2e8ee942734de74b95cc518348431307af
Yuvraj Sakshith <ysakshith@gmail.com>
2023-05-16 12:57:24 +0530

    setup kernel initialisation log messages

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -45,6 +45,8 @@ void change_to_kernel_page_directory(){
 
 void kernel_start(){
 
+    kclear_display(); // clear the screen
+
     __disable_irq();
 
 #if defined(VGA)
@@ -55,53 +57,88 @@ void kernel_start(){
     memset(_gdt, 0, sizeof(struct  gdt));
     get_gdt_from_structured(_gdt, _gdt_s, GDT_ENTRIES);
 
+    kputs("[KERNEL] setting up gdt.\n");
+
     // load gdt
     load_gdt(_gdt, sizeof(_gdt));
+
+    kputs("[KERNEL] loaded up gdt.\n");
 
     // form tss
     memset(&_tss, 0, sizeof(_tss));
     _tss.esp0 = 0x600000;  
     _tss.ss0  = KERNEL_DATA_SELECTOR;
 
+    kputs("[KERNEL] setting up tss.\n");
+
+
     // load tss
     tss_load(0x28); // 0x28 is the offset of tss in gdt
 
+    kputs("[KERNEL] loaded up tss.\n");
 
-    kclear_display(); // clear the screen
+
     idt_init(); // initialise interrupt descriptor table
+
+    kputs("[KERNEL] initialised idt. \n");
+
     __0x80_init(); // initialise 0x80 int
+
+    kputs("[KERNEL] initialised gammaOS system call handlers. \n");
 
     kheap_init(); // initialise kernel heap section
 
+    kputs("[KERNEL] initialised kernel heap. \n");
+
     keyboard_init();// intialise the abstract keyboard driver
+
+    kputs("[KERNEL] initialised keyboard drivers. \n");
 
     /* creating kernel address space and loading its page directory into cr3*/
     kernel_space = create_virtual_address_space(PAGE_PRESENT | PAGE_USER_ACCESS | PAGE_WRITE_ACCESS);
     if(kernel_space == 0x0){
         kpanic("[KERNEL ERROR]: Couldn't create kernel address space...\n");
     }
+
+    kputs("[KERNEL] initialised kernel address space. \n");
+
     load_page_directory(kernel_space->pd);
 
+    kputs("[KERNEL] loaded kernel page directory. \n");
+
     file_init(); // initialise the virtual file system layer
+
+    kputs("[KERNEL] initialised virtual filesystem. \n");
+
     if(vfs_attach(fat16_init()) < 0){// attach fat16 fs into the primary disk
         kpanic("[KERNEL ERROR]: Couldn't attach filesystem to primary disk");
     } 
+
+    kputs("[KERNEL] attached fat16 driver. \n");
 
     if(!disk_init('A')){
         kpanic("[KERNEL ERROR]: Couldn't initialise primary disk...\n");
     }
 
+    kputs("[KERNEL] initialised primary disk. \n");
+
     enable_paging(); // enable paging
+
+    kputs("[KERNEL] initialised paging. \n");
 
 
     // set-up gshell
     struct process* proc_gshell = process_new("A:/usrspc/gshell/main.o");
+
+    kputs("[KERNEL] loading gshell...\n");
 
     if(!proc_gshell){
         kpanic("process not created!!:(\n");
     }   
 
     __enable_irq(); // enable interrupts
+
+    kclear_display();
 
     gammaos_first_ever_task(); // run the shell
 
